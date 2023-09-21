f83b6256b6f9b83b95998067b55d87485be2a85a
Russell Damerell-Moss <karoofish@gmail.com>
2023-09-21 17:26:42 +0200

    moved keymap rustrunnables to only activate on lsp for rust

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps hereafter
-vim.keymap.set("n", "<A-r>", "<cmd>RustRunnables<cr>", { desc = "Rust Runnables" })
