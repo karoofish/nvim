60138a85fe72fab875cbdd7ae9c01bee1f70853c
Russell Damerell-Moss <karoofish@gmail.com>
2024-01-11 15:13:30 +0200

    removed old unneeded config

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config.lazy") -- kanagawa as a backup, `recall()` can return `nil`.
-local theme = require("last-color").recall() or "catppuccin"
+local theme = require("last-color").recall() or "desert"
 vim.cmd(("colorscheme %s"):format(theme))
+vim.api.nvim_set_hl(0, "@lsp.mod.mutable.rust", { underline = true })
